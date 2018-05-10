fe1841924677ca53017ccbd94ae5b5e55d3bb2c3
nazariy17 <nazariy2410@gmail.com>
2018-05-10 11:21:54 +0300

    POPULAR LAKES

--- src/js/front.js
+++ src/js/front.js
@@ -1,9 +1,25 @@
 let front = {
 
+    slider_options_default: {
+        wrapAround: true,
+        pageDots: false,
+        prevNextButtons: true,
+        autoPlay: true,
+        cellAlign: 'left',
+        contain: true,
+    },
 
     init: function () {
         this.events();
         this.rangeInit();
+        this.newSlider('.app-popular__carousel');
+    },
+
+    newSlider: function (selector, options) {
+        options = (options !== undefined) ? Object.assign({}, this.slider_options_default, options) : this.slider_options_default;
+        // let carousel = new Flickity(document.querySelector(selector), options);
+
+        return new Flickity(document.querySelector(selector), options);
     },
 
     getVals: function () {
